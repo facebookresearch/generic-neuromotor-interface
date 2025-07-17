1c6b7ff94e64a08f7563918f6d2e6c2db62e45d7
Collin Schlager <cschlager@meta.con>
2025-07-17 18:45:35 +0000

    short report

--- run_all_int_tests.sh
+++ run_all_int_tests.sh
@@ -3,25 +3,25 @@ set -x
 
 # USE CUDA TRUE
 # USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration # don't waste time on full + fake checkpoint
-USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -s
-USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -s
-USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -s  # should error 
-USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration -s # should error
-USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -s
-USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -s
+USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -rAV
+USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -rAV
+USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -rAV  # should error 
+USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration -rAV # should error
+USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -rAV
+USE_CUDA=True USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -rAV
 
 # USE CUDA FALSE
 # USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration # don't waste time on full + fake checkpoint
-USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -s
-USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -s
-USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -s # should error
-USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration -s # should error
-USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -s
-USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -s
+USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -rAV
+USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -rAV
+USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -rAV # should error
+USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=True USE_REAL_CHECKPOINTS=False pytest -m integration -rAV # should error
+USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=True pytest -m integration -rAV
+USE_CUDA=False USE_REAL_DATA=False USE_FULL_DATA=False USE_REAL_CHECKPOINTS=False pytest -m integration -rAV
 
 
 # EXTRA EXPENSIVE ONES
 
-USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -s  # expensive
+USE_CUDA=True USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -rAV  # expensive
 
-USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -s # very expensive
+USE_CUDA=False USE_REAL_DATA=True USE_FULL_DATA=True USE_REAL_CHECKPOINTS=True pytest -m integration -rAV # very expensive
