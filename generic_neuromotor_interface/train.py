54abe499b2704e9844a4e6fb2a56a2f0acd10afa
Collin Schlager <cschlager@meta.com>
2025-07-20 14:00:54 -0700

    Merge evaluation logic

--- generic_neuromotor_interface/train.py
+++ generic_neuromotor_interface/train.py
@@ -92,6 +92,7 @@ def train(
             results=results,
             logger=logger,
             accelerator=accelerator,
+            config=config,
         )
 
     _log_pretty_results(results=results, logger=logger)
@@ -128,6 +129,57 @@ def evaluate_from_checkpoint(
     results = {}
     results["checkpoint_path"] = checkpoint_path
 
+    results = _run_validate_and_test(
+        module=module,
+        datamodule=datamodule,
+        results=results,
+        logger=logger,
+        accelerator=accelerator,
+        config=config,
+        evaluate_validation_set=evaluate_validation_set,
+        evaluate_test_set=evaluate_test_set,
+    )
+
+    return results
+
+
+@pl.utilities.rank_zero_only
+def _load_and_report_best_checkpoint(
+    trainer, module, results, logger
+) -> pl.LightningModule:
+    logger.info("Loading best checkpoint...")
+
+    checkpoint_callback = trainer.checkpoint_callback
+    if checkpoint_callback is None:
+        raise RuntimeError("No checkpoint callback found in trainer")
+
+    best_checkpoint_path = checkpoint_callback.best_model_path
+    best_checkpoint_score = checkpoint_callback.best_model_score
+
+    logger.info(
+        f"Loading best checkpoint from {best_checkpoint_path=} "
+        f"with {best_checkpoint_score=}..."
+    )
+    module = module.__class__.load_from_checkpoint(best_checkpoint_path)
+
+    results["best_checkpoint_path"] = best_checkpoint_path
+    results["best_checkpoint_score"] = best_checkpoint_score.cpu().item()
+
+    return module, results
+
+
+@pl.utilities.rank_zero_only
+def _run_validate_and_test(
+    module,
+    datamodule,
+    results,
+    logger,
+    accelerator,
+    config,
+    evaluate_validation_set=True,
+    evaluate_test_set=True,
+):
+
     trainer = Trainer(
         accelerator=accelerator,
         devices=1,
@@ -160,56 +212,6 @@ def evaluate_from_checkpoint(
         logger.info(f"Test completed! {test_results=}")
 
         results["test_metrics"] = test_results
-
-    return results
-
-
-@pl.utilities.rank_zero_only
-def _load_and_report_best_checkpoint(
-    trainer, module, results, logger
-) -> pl.LightningModule:
-    logger.info("Loading best checkpoint...")
-
-    checkpoint_callback = trainer.checkpoint_callback
-    if checkpoint_callback is None:
-        raise RuntimeError("No checkpoint callback found in trainer")
-
-    best_checkpoint_path = checkpoint_callback.best_model_path
-    best_checkpoint_score = checkpoint_callback.best_model_score
-
-    logger.info(
-        f"Loading best checkpoint from {best_checkpoint_path=} "
-        f"with {best_checkpoint_score=}..."
-    )
-    module = module.__class__.load_from_checkpoint(best_checkpoint_path)
-
-    results["best_checkpoint_path"] = best_checkpoint_path
-    results["best_checkpoint_score"] = best_checkpoint_score.cpu().item()
-
-    return module, results
-
-
-@pl.utilities.rank_zero_only
-def _run_validate_and_test(module, datamodule, results, logger, accelerator):
-    logger.info("Running validate and test w/ devices=1 only...")
-
-    trainer = Trainer(
-        accelerator=accelerator,
-        devices=1,
-    )
-
-    logger.info("Running validation...")
-    val_results = trainer.validate(model=module, datamodule=datamodule)
-    logger.info(f"Validation completed! {val_results=}")
-
-    # TODO: do we need to enforce cpu here as well?
-    # TODO: share logic with evaluate_from_checkpoint?
-    logger.info("Running test...")
-    test_results = trainer.test(model=module, datamodule=datamodule)
-    logger.info(f"Test completed! {test_results=}")
-
-    results["val_metrics"] = val_results
-    results["test_metrics"] = test_results
 
     return results
 
